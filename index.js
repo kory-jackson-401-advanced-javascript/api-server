ac57a0d2c70904ba65ccb596622497852dee6a3f
Dirrbick <kory.a.jaxn@gmail.com>
2020-12-20 16:30:35 -0800

    refactored all to support mongo

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict';
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 const server = require('./lib/server.js');
 
 server.start(process.env.PORT);
